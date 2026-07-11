4d28446c4889bd7becf6ae5410b3cdd99461ed6a
Ad-rian27 <rian270627@gmail.com>
2026-07-11 10:28:34 +0530

    Added ViewStudents component

---
+++ src/components/ViewStudents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ViewStudents = () => {
+    return (
+        <div>
+
+            <h1>View Students Page</h1>
+
+        </div>
+    )
+}
+
+export default ViewStudents
